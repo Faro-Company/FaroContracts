9217621ad327276fe5b793216b16d9d855d16d59
user409 <cevahir@avalabs.org>
2021-11-18 00:17:17 +0300

    offering automated USDC testing contracts

--- test/OfferableTokenVaultFactory-test.js
+++ test/OfferableTokenVaultFactory-test.js
@@ -11,7 +11,7 @@ const LIST_PRICE = 5;
 const PARTICIPANTS_NUM = 100;
 
 
-describe('OfferableERC721TokenVault', function () {
+describe('OfferableTokenVaultFactory', function () {
 
     before(async function () {
         const accounts = await ethers.provider.listAccounts();
@@ -70,53 +70,17 @@ describe('OfferableERC721TokenVault', function () {
 
         this.contractWithSigner = this.ercVaultFactory.connect(this.ownerSigner);
 
+    });
+
+    it('Can deploy offering contract successfully', async function () {
         const tx = await contractWithSigner.mint(this.ktlOwnershipToken.address, this.user, this.owner,
             this.supply, LIST_PRICE, NAME, SYMBOL, this.funderAddresses, this.allocations);
         await tx.wait();
-
-        this.offeringAddress = await this.contractWithSigner.getVault(0);
+        expect(await this.contractWithSigner.getNumOfTokens()).to.equal(1);
+        const offeringAddress = await this.contractWithSigner.getVault(0);
         const OfferingVault = await ethers.getContractFactory('OfferableERC721TokenVault');
-        const offeringVault = await OfferingVault.attach(this.offeringAddress);
-        this.ownerSigner = offeringVault.connect(this.owner);
-        this.nonOwnerSigner = offeringVault.connect(this.distributor);
-
-        this.participantSigner = offeringVault.connect(this.offeringParticipants[0]);
-
-        const usdc = {
-            address: "0x68ec573C119826db2eaEA1Efbfc2970cDaC869c4",
-            abi: [
-                "function gimmeSome() external",
-                "function balanceOf(address _owner) public view returns (uint256 balance)",
-                "function transfer(address _to, uint256 _value) public returns (bool success)",
-            ],
-        };
-
-        const usdcContract = new ethers.Contract(usdc.address, usdc.abi, this.nonOwnerSigner);
-        const mintUSDCTx = await usdcContract.gimmeSome();
-        await mintUSDCTx.wait()
-
-
-
-    });
-
-    it('Cannot be started by non-owner', async function () {
-        await expectRevert(this.nonOwnerSigner.start());
-    });
-
-    it('Can be started by the owner', async function() {
-        const startTx = await this.ownerSigner.start();
-        await startTx.wait();
-        expect(await this.ownerSigner.getOfferingState()).to.equal(1);
-    })
-
-    it('Cannot bid if not among funders', async function() {
-        await expectRevert(this.nonOwnerSigner.bid(100, {value: 1000}));
-    });
-
-    it('Cannot bid more than allocation', async function () {
-        this.ktlOwnershipToken.connect(this.ownerSigner);
-        let tx = await this.ktlOwnershipToken.transferFrom(this.owner, this.user, TOKEN_ID);
-        await tx.wait();
-        expect(await this.ktlOwnershipToken.ownerOf(TOKEN_ID)).to.equal(this.user);
+        const offeringVault = await OfferingVault.attach(offeringAddress);
+        const ownerSigner = offeringVault.connect(this.owner);
+        expect(await ownerSigner.getOfferingState()).to.equal(0);
     });
 });
