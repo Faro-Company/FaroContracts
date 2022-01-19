f8322c4188918944ae608315550f98d1becf3a89
cevo <cevo@pop-os.localdomain>
2022-01-20 00:11:19 +0300

    added get holder balances batch to faro token

--- contracts/FaroToken.sol
+++ contracts/FaroToken.sol
@@ -9,4 +9,12 @@ contract FaroToken is ERC20 {
     constructor() ERC20("Faro Studio Token", "FARO") {
         _mint(msg.sender, 244000000 * 10**uint(decimals()));
     }
+
+    function balancesOf(address[] holders) public view returns (uint[]) {
+        address[] memory result = new address[](holders.length);
+        for (uint i = 0; i < holders.length; i++) {
+            result[i] = this.balanceOf(holders[i]);
+        }
+        return result;
+    }
 }
